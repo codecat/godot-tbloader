a93b686a786677dadc5201a0ff761a5b0272a941
Melissa Geels <spansjh@gmail.com>
2022-02-27 16:27:31 +0100

    First code

---
+++ SConstruct
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import sys
+
+env = SConscript("godot-cpp/SConstruct")
+
+# For the reference:
+# - CCFLAGS are compilation flags shared between C and C++
+# - CFLAGS are for C-specific compilation flags
+# - CXXFLAGS are for C++-specific compilation flags
+# - CPPFLAGS are for pre-processor flags
+# - CPPDEFINES are for pre-processor defines
+# - LINKFLAGS are for linking flags
+
+# tweak this if you want to use different folders, or more folders, to store your source code in.
+env.Append(CPPPATH=["src/"])
+sources = Glob("src/*.cpp")
+
+if env["platform"] == "osx":
+	library = env.SharedLibrary(
+		"bin/libtbloader.{}.{}.framework/libtbloader.{}.{}".format(
+			env["platform"], env["target"], env["platform"], env["target"]
+		),
+		source=sources,
+	)
+else:
+	library = env.SharedLibrary(
+		"bin/tbloader.{}.{}.{}{}".format(
+			env["platform"], env["target"], env["arch_suffix"], env["SHLIBSUFFIX"]
+		),
+		source=sources,
+	)
+
+Default(library)
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,65 @@
-//TODO
+#include <godot/gdnative_interface.h>
+
+#include <godot_cpp/godot.hpp>
+
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/core/binder_common.hpp>
+
+#include <godot_cpp/classes/node3d.hpp>
+
+#include <godot_cpp/variant/utility_functions.hpp>
+
+using namespace godot;
+
+class TBLoader : public Node3D
+{
+	GDCLASS(TBLoader, Node3D);
+
+protected:
+	static void _bind_methods()
+	{
+		ClassDB::bind_method(D_METHOD("do_something"), &TBLoader::do_something);
+	}
+
+public:
+	TBLoader()
+	{
+		UtilityFunctions::print("TBLoader constructor");
+	}
+
+	~TBLoader()
+	{
+		UtilityFunctions::print("TBLoader denstructor");
+	}
+
+	void do_something()
+	{
+		UtilityFunctions::print("TBLoader do_something was called!");
+	}
+};
+
+void register_tbloader_types()
+{
+	ClassDB::register_class<TBLoader>();
+}
+
+void unregister_tbloader_types()
+{
+}
+
+extern "C"
+{
+	GDNativeBool GDN_EXPORT tbloader_init(
+		const GDNativeInterface *p_interface,
+		const GDNativeExtensionClassLibraryPtr p_library,
+		GDNativeInitialization *r_initialization
+	) {
+		GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+
+		init_obj.register_scene_initializer(register_tbloader_types);
+		init_obj.register_scene_terminator(unregister_tbloader_types);
+
+		return init_obj.init();
+	}
+}
