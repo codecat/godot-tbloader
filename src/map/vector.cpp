7d5979918c3c5db870f21bf45ac503429cef4758
Melissa Geels <spansjh@gmail.com>
2022-03-02 03:16:39 +0100

    Updated vec3 math to use const references

--- src/map/vector.cpp
+++ src/map/vector.cpp
@@ -1,15 +1,16 @@
 #include "vector.h"
 
 #include <math.h>
+#include <string.h>
 
 #include "libmap_math.h"
 #include "matrix.h"
 
-bool vec3_equals(vec3 lhs, vec3 rhs) {
+bool vec3_equals(const vec3& lhs, const vec3& rhs) {
 	return vec3_length(vec3_sub(lhs, rhs)) < CMP_EPSILON;
 }
 
-vec3 vec3_add(vec3 lhs, vec3 rhs) {
+vec3 vec3_add(const vec3& lhs, const vec3& rhs) {
 	return {
 		lhs.x + rhs.x,
 		lhs.y + rhs.y,
@@ -17,7 +18,7 @@ vec3 vec3_add(vec3 lhs, vec3 rhs) {
 	};
 }
 
-vec3 vec3_sub(vec3 lhs, vec3 rhs) {
+vec3 vec3_sub(const vec3& lhs, const vec3& rhs) {
 	return {
 		lhs.x - rhs.x,
 		lhs.y - rhs.y,
@@ -25,7 +26,7 @@ vec3 vec3_sub(vec3 lhs, vec3 rhs) {
 	};
 }
 
-vec3 vec3_mul(vec3 lhs, vec3 rhs) {
+vec3 vec3_mul(const vec3& lhs, const vec3& rhs) {
 	return {
 		lhs.x * rhs.x,
 		lhs.y * rhs.y,
@@ -33,7 +34,7 @@ vec3 vec3_mul(vec3 lhs, vec3 rhs) {
 	};
 }
 
-vec3 vec3_div(vec3 lhs, vec3 rhs) {
+vec3 vec3_div(const vec3& lhs, const vec3& rhs) {
 	return {
 		lhs.x / rhs.x,
 		lhs.y / rhs.y,
@@ -41,25 +42,25 @@ vec3 vec3_div(vec3 lhs, vec3 rhs) {
 	};
 }
 
-double vec3_sqlen(vec3 v) {
+double vec3_sqlen(const vec3& v) {
 	return vec3_dot(v, v);
 }
 
-double vec3_length(vec3 v) {
+double vec3_length(const vec3& v) {
 	double sqlen = vec3_sqlen(v);
 	return (double)sqrt(vec3_sqlen(v));
 }
 
-vec3 vec3_normalize(vec3 v) {
+vec3 vec3_normalize(const vec3& v) {
 	double len = vec3_length(v);
 	return vec3_div_double(v, len);
 }
 
-double vec3_dot(vec3 lhs, vec3 rhs) {
+double vec3_dot(const vec3& lhs, const vec3& rhs) {
 	return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z;
 }
 
-vec3 vec3_cross(vec3 lhs, vec3 rhs) {
+vec3 vec3_cross(const vec3& lhs, const vec3& rhs) {
 	return {
 		lhs.y * rhs.z - lhs.z * rhs.y,
 		lhs.z * rhs.x - lhs.x * rhs.z,
@@ -67,27 +68,43 @@ vec3 vec3_cross(vec3 lhs, vec3 rhs) {
 	};
 }
 
-vec3 vec3_rotate(vec3 v, vec3 axis, double angle) {
+vec3 vec3_rotate(const vec3& v, const vec3& axis, double angle) {
 	mat4 mat = rotation_matrix(axis, angle);
 	return mat4_mul_vec3(mat, v);
 }
 
-vec3 vec3_add_double(const vec3 lhs, const double rhs) {
+vec3 vec3_add_double(const vec3& lhs, const double rhs) {
 	vec3 result = { lhs.x + rhs, lhs.y + rhs, lhs.z + rhs };
 	return result;
 };
 
-vec3 vec3_sub_double(const vec3 lhs, const double rhs) {
+vec3 vec3_sub_double(const vec3& lhs, const double rhs) {
 	vec3 result = { lhs.x - rhs, lhs.y - rhs, lhs.z - rhs };
 	return result;
 };
 
-vec3 vec3_mul_double(const vec3 lhs, const double rhs) {
+vec3 vec3_mul_double(const vec3& lhs, const double rhs) {
 	vec3 result = { lhs.x * rhs, lhs.y * rhs, lhs.z * rhs };
 	return result;
 };
 
-vec3 vec3_div_double(const vec3 lhs, const double rhs) {
+vec3 vec3_div_double(const vec3& lhs, const double rhs) {
 	vec3 result = { lhs.x / rhs, lhs.y / rhs, lhs.z / rhs };
 	return result;
+};
+
+vec3 vec3_parse(const char* str) {
+	vec3 ret;
+
+	ret.x = atof(str);
+	const char* token = strchr(str, ' ');
+	if (token != nullptr) {
+		ret.y = atof(token + 1);
+		token = strchr(token + 1, ' ');
+		if (token != nullptr) {
+			ret.z = atof(token + 1);
+		}
+	}
+
+	return ret;
 };
--- src/map/vector.h
+++ src/map/vector.h
@@ -6,22 +6,24 @@ struct vec3 {
 	double z;
 };
 
-bool vec3_equals(vec3 lhs, vec3 rhs);
-vec3 vec3_add(vec3 lhs, vec3 rhs);
-vec3 vec3_sub(vec3 lhs, vec3 rhs);
-vec3 vec3_mul(vec3 lhs, vec3 rhs);
-vec3 vec3_div(vec3 lhs, vec3 rhs);
+bool vec3_equals(const vec3& lhs, const vec3& rhs);
+vec3 vec3_add(const vec3& lhs, const vec3& rhs);
+vec3 vec3_sub(const vec3& lhs, const vec3& rhs);
+vec3 vec3_mul(const vec3& lhs, const vec3& rhs);
+vec3 vec3_div(const vec3& lhs, const vec3& rhs);
 
-vec3 vec3_add_double(const vec3 lhs, const double rhs);
-vec3 vec3_sub_double(const vec3 lhs, const double rhs);
-vec3 vec3_mul_double(const vec3 lhs, const double rhs);
-vec3 vec3_div_double(const vec3 lhs, const double rhs);
+vec3 vec3_add_double(const vec3& lhs, double rhs);
+vec3 vec3_sub_double(const vec3& lhs, double rhs);
+vec3 vec3_mul_double(const vec3& lhs, double rhs);
+vec3 vec3_div_double(const vec3& lhs, double rhs);
 
-double vec3_dot(vec3 lhs, vec3 rhs);
-vec3 vec3_cross(vec3 lhs, vec3 rhs);
-double vec3_sqlen(vec3 v);
-double vec3_length(vec3 v);
-vec3 vec3_normalize(vec3 v);
-vec3 vec3_rotate(vec3 v, vec3 axis, double angle);
+double vec3_dot(const vec3& lhs, const vec3& rhs);
+vec3 vec3_cross(const vec3& lhs, const vec3& rhs);
+double vec3_sqlen(const vec3& v);
+double vec3_length(const vec3& v);
+vec3 vec3_normalize(const vec3& v);
+vec3 vec3_rotate(const vec3& v, const vec3& axis, double angle);
+
+vec3 vec3_parse(const char* str);
 
 #endif
