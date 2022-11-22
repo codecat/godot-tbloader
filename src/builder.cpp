bcd07e90deb2af6c598405e8e83bd6c6b1691d2a
levrault <lucf.langis@gmail.com>
2022-11-22 14:56:22 -0500

    feat(builder): directly use m_texture_path

--- src/builder.cpp
+++ src/builder.cpp
@@ -459,7 +459,7 @@ MeshInstance3D* Builder::build_entity_mesh(int idx, LMEntity& ent, Node3D* paren
 		}
 
 		// Attempt to load material
-		material = material_from_name(m_loader->m_texture_path, tex.name);
+		material = material_from_name(tex.name);
 
 		if (material == nullptr) {
 			// Load texture
@@ -553,7 +553,7 @@ void Builder::load_and_cache_map_textures()
 
 		// Find the texture with a supported extension - stop when it can be loaded
 		for (int ext_i = 0; ext_i < num_extensions; ext_i++) {
-			tex_path = texture_path(m_loader->m_texture_path, tex.name, supported_extensions[ext_i]);
+			tex_path = texture_path(tex.name, supported_extensions[ext_i]);
 			if (resource_loader->exists(tex_path, "CompressedTexture2D")) {
 				m_loaded_map_textures[tex.name] = resource_loader->load(tex_path);
 				has_loaded_texture = true;
@@ -570,14 +570,14 @@ void Builder::load_and_cache_map_textures()
 	}
 }
 
-String Builder::texture_path(const String& texture_path, const char* name, const char* extension)
+String Builder::texture_path(const char* name, const char* extension)
 {
-	return texture_path + "/" + name + "." + extension;
+	return m_loader->m_texture_path + "/" + name + "." + extension;
 }
 
-String Builder::material_path(const String& texture_path, const char* name)
+String Builder::material_path(const char* name)
 {
-	auto root_path = texture_path + name;
+	auto root_path = m_loader->m_texture_path + name;
 	String material_path;
 
 	if (FileAccess::file_exists(root_path + ".material")) {
@@ -597,9 +597,9 @@ Ref<Texture2D> Builder::texture_from_name(const char* name)
 	return VariantCaster<Ref<Texture2D>>::cast(m_loaded_map_textures[name]);
 }
 
-Ref<Material> Builder::material_from_name(const String& texture_path, const char* name)
+Ref<Material> Builder::material_from_name(const char* name)
 {
-	auto path = material_path(texture_path, name);
+	auto path = material_path(name);
 
 	auto resource_loader = ResourceLoader::get_singleton();
 	if (!resource_loader->exists(path)) {
