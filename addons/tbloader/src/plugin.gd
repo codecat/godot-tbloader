192fde116a6d32e7e21da2781c597c76f1a30d14
Jake Cattrall <krazyjakee@gmail.com>
2022-12-27 19:35:11 +0000

    Build fgd button (#1)
    * add build fgd button
    
    * working loader

--- addons/tbloader/src/plugin.gd
+++ addons/tbloader/src/plugin.gd
@@ -2,7 +2,9 @@
 extends EditorPlugin
 class_name TBPlugin
 
+var godot_fgd = preload("res://addons/tbloader/src/godot-fgd/toFgd.gd").new()
 var map_control: Control = null
+var fgd_control: Control = null
 var editing_loader: WeakRef = weakref(null)
 
 func _enter_tree():
@@ -12,6 +14,10 @@ func _enter_tree():
 	map_control.set_visible(false)
 	add_control_to_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU, map_control)
 
+	fgd_control = create_fgd_control()
+	fgd_control.set_visible(false)
+	add_control_to_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU, fgd_control)
+
 func _exit_tree():
 	set_icons(false)
 
@@ -19,11 +25,16 @@ func _exit_tree():
 	map_control.queue_free()
 	map_control = null
 
+	remove_control_from_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU, fgd_control)
+	fgd_control.queue_free()
+	fgd_control = null
+
 func _handles(object):
 	return object is TBLoader
 
 func _make_visible(visible: bool):
 	map_control.set_visible(visible)
+	fgd_control.set_visible(visible)
 
 func _edit(object):
 	editing_loader = weakref(object)
@@ -37,6 +48,20 @@ func create_map_control() -> Control:
 	var ret = HBoxContainer.new()
 	ret.add_child(button_build_meshes)
 	return ret
+
+func create_fgd_control() -> Control:
+	var button_build_fgd = Button.new()
+	button_build_fgd.flat = true
+	button_build_fgd.text = "Build FGD"
+	button_build_fgd.connect("pressed", Callable(self, "build_fgd"))
+
+	var ret = HBoxContainer.new()
+	ret.add_child(button_build_fgd)
+	return ret
+
+func build_fgd():
+	var loader = editing_loader.get_ref()
+	godot_fgd.build(loader.entity_path.replace("res://", ""))
 
 func build_meshes():
 	var loader = editing_loader.get_ref()
